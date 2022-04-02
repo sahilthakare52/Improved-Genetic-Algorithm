461da9d9b92a42b5d253f9a9f29f5522ac3244de
Sahil Thakare <sahilthakare@Sahils-MacBook-Pro.local>
2022-04-02 11:23:10 +0530

    final code

--- Untitled-1.py
+++
@@ -1,8 +0,0 @@
-import itertools
-
-
-for i, j in itertools.zip_longest(range(0, 10, 2), range(1,10,2)):
-    print(i)
-    print(j)
-    
-   
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import itertools
 
 print ( "Gathering the system resource information........\n ==============#################===============" )
 
+os.system("rm  Next_iteration1.txt>/dev/null 2>&1")
+os.system("rm  Next_iteration2.txt>/dev/null 2>&1")
+os.system("rm  Next_iteration3.txt>/dev/null 2>&1")
 data = os.system(" echo '' > mydata.txt; for i in `cat servers.txt`;do ssh  ubuntu@$i 'hostname && sudo sh /etc/update-motd.d/50-landscape-sysinfo' ; echo '====='; done | tee mydata.txt ")
 
 print(" Fetching the current memory utilisation..........\n")
@@ -31,9 +34,11 @@ print("\n==============#################===============")
 print("Iteration - 1 ................. Starting\n ")
 print("First Generation - Parents...........\n")
 
-population = os.system("cat servers.txt |wc -l")
+# population = os.system("cat servers.txt |wc -l")
 
-print("The hosts in the selected population is: {} ".format(population))
+count = len(open("servers.txt").readlines())
+
+print("The hosts in the selected population is: {} ".format(count))
 
 print("==============#################===============")
 
@@ -45,42 +50,72 @@ print("Dividing the population into tournament participants ...........\n")
 os.system("cat mydata.txt | grep Memory | cut -d 'I' -f1| sort | tee Memory.txt >/dev/null 2>&1")
 os.system("cat mydata.txt | grep Memory | sort | tee ip_add.txt >/dev/null 2>&1")
 
-def update_next_iteration(selections):
-        fil=open("next_iteration_list.txt", "a")
+def update_next_iteration(selections, nextiteration):
+        fil=open(nextiteration, "a")
         fil.write(selections + "\n")
         fil.close
 
 
-def tourament_func(n, filename):
-    for i, j in itertools.zip_longest(range(0, 10, 2), range(1,10,2)):
-      with open(filename) as f:
-       firstline = f.readlines()[i].rstrip()
-      with open("ip_add.txt") as f:
-       firstline_ip_add = f.readlines()[i].rstrip()
-      with open(filename) as f:
-       secondline = f.readlines()[j].rstrip()
-      with open("ip_add.txt") as f:
-       secondline_ip_add = f.readlines()[j].rstrip()
-       print(firstline)
-       print(secondline)
+def tourament_func(n, filename, updatedip, nextiteration):
+    for i, j in itertools.zip_longest(range(0, count, 2), range(1,count,2)):
+      if i is not None:
+       with open(filename) as f:
+        firstline = f.readlines()[i].rstrip()
+       with open(updatedip) as f:
+        firstline_ip_add = f.readlines()[i].rstrip()
+      if j is not None:
+       with open(filename) as f:
+        secondline = f.readlines()[j].rstrip()
+       with open(updatedip) as f:
+        secondline_ip_add = f.readlines()[j].rstrip()
+      print(firstline)
+      print(secondline)
 
-       print ("======================")
+      print ("======================")
 
-       print ("Running tournament --> iteration {}".format(n))
-       if firstline>secondline:
+      print ("Running tournament --> iteration {}".format(n))
+      if firstline>secondline:
           print("Memory  utilisation for server {} is greater than server {}\n Server {} is selected".format(i, j, j))
-          update_next_iteration(secondline_ip_add)
-       elif firstline<secondline:
+          update_next_iteration(secondline_ip_add, nextiteration)
+      elif firstline<secondline:
           print("Memory  utilisation for server {} is less than server {}\n Server {} is Selected ".format(i, j, i))
-          update_next_iteration(firstline_ip_add)
-       else:
+          update_next_iteration(firstline_ip_add, nextiteration)
+      else:
           print("Memory  utilisation for server {} is equal to server {}\n Server will be selected randomly".format(i, j))
-          update_next_iteration(firstline_ip_add)
-       print ("==========================================")
+          update_next_iteration(firstline_ip_add, nextiteration)
+      print ("==========================================")
  
-tourament_func(1, "Memory.txt")
+tourament_func(1, "Memory.txt", "ip_add.txt", "Next_iteration1.txt")
 
 print ("Selected servers for next iteration")
-os.system("cat next_iteration_list.txt")
+os.system("cat Next_iteration1.txt")
+
+print ("==========================================")
+
+print ("Starting Next iteration..........")
+
+count = len(open("Next_iteration1.txt").readlines())
+
+tourament_func(2, "Next_iteration1.txt", "Next_iteration1.txt", "Next_iteration2.txt")
+
+print ("Selected servers for next iteration")
+os.system("cat Next_iteration2.txt")
 
 
+print ("Starting Next iteration..........")
+
+count = len(open("Next_iteration2.txt").readlines())
+
+tourament_func(2, "Next_iteration2.txt", "Next_iteration2.txt", "Next_iteration3.txt")
+
+print ("Selected servers for next iteration")
+os.system("cat Next_iteration3.txt")
+
+print ("Starting Next iteration..........")
+
+count = len(open("Next_iteration3.txt").readlines())
+
+tourament_func(2, "Next_iteration3.txt", "Next_iteration3.txt", "Next_iteration4.txt")
+
+print ("Selected servers for next iteration")
+os.system("cat Next_iteration4.txt")
